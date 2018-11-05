aa5677313e2e0ef4ec111518f767e272be993016
James Biswell <james.biswell@endeavourhealth.org>
2018-11-05 11:28:29 +0000

    James test commit.

--- src/scheduler/src/main/java/org/endeavourhealth/scheduler/Main.java
+++ src/scheduler/src/main/java/org/endeavourhealth/scheduler/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        // test comments
+        // test comment
         if (args.length == 0) {
             // Run the whole process
             generateData();
