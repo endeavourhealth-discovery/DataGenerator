518bf09cc42a9b1651630c453813cdf07f4e9b2f
James Biswell <james.biswell@endeavourhealth.org>
2018-11-05 15:51:34 +0000

    First commit on SendCSVFilesSFTP, using java.nio to read from and to a directory

--- src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/SendCsvFilesSFTP.java
+++ src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/SendCsvFilesSFTP.java
@@ -4,17 +4,33 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.io.*;
+// import java.net.URI;
+import java.nio.file.*;
 
 public class SendCsvFilesSFTP implements Job {
 
     private static final Logger LOG = LoggerFactory.getLogger(SendCsvFilesSFTP.class);
 
+
+
     public void execute(JobExecutionContext jobExecutionContext) {
 
-        System.out.println("Sending Encrypted CSV files");
+    System.out.println("Sending Encrypted CSV files");
 
-        //TODO Send encrypted CSV files via SFTP
-
+    //TODO Send encrypted CSV files via SFTP
+    try {
+        // URI sourceURI = URI.create("");
+        // URI targetURI = URI.create("");
+        Path sourcePath = Paths.get("C:/source");
+        Path targetPath = Paths.get("C:/target");
+        Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+        // Files.walkFileTree(sourcePath, new SimpleFileVisitor<>());
         System.out.println("CSV files sent");
     }
+    catch (IOException e){
+        System.out.println("I/O error occurred" + " " + e);
+    }
+    // System.out.println("CSV files sent");
+}
 }
