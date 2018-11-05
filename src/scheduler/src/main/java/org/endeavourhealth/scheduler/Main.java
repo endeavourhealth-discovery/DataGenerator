bcf90ddd98021463031595bb1ea9c6377cd45920
James Biswell <james.biswell@endeavourhealth.org>
2018-11-05 10:43:24 +0000

    Test commit, comment only will not break anything!

--- src/scheduler/src/main/java/org/endeavourhealth/scheduler/Main.java
+++ src/scheduler/src/main/java/org/endeavourhealth/scheduler/Main.java
@@ -20,6 +20,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        // james's comment
         if (args.length == 0) {
             // Run the whole process
             generateData();
