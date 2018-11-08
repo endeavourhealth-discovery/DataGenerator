9ed6b27458aa5cd4dddb55626a1e8dd378fc6482
Froiland Go <Froiland.Go@learning-health.co.uk>
2018-11-08 16:06:11 +0000

    Added logs and exception handling, JUnit

--- src/scheduler/src/main/java/org/endeavourhealth/scheduler/util/PgpEncryptDecrypt.java
+++ src/scheduler/src/main/java/org/endeavourhealth/scheduler/util/PgpEncryptDecrypt.java
@@ -6,99 +6,96 @@ import org.bouncycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
 import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
 import org.bouncycastle.cms.jcajce.JceKeyTransRecipient;
 import org.bouncycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.OutputEncryptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.KeyStore;
 import java.security.PrivateKey;
-import java.security.Security;
 import java.security.cert.CertificateEncodingException;
-import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Collection;
 
 public class PgpEncryptDecrypt {
 
-    public static void encryptFile(File file, X509Certificate encryptionCertificate, String provider)
-            throws CertificateEncodingException, CMSException, IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(PgpEncryptDecrypt.class);
 
-        byte[] data = IOUtils.toByteArray(new FileInputStream(file));
-        if (data != null && encryptionCertificate != null) {
+    public static void encryptFile(File file,
+                                      X509Certificate encryptionCertificate,
+                                      String provider) throws Exception {
 
-            CMSEnvelopedDataGenerator cmsEnvelopedDataGenerator = new CMSEnvelopedDataGenerator();
-            JceKeyTransRecipientInfoGenerator jceKey = new JceKeyTransRecipientInfoGenerator(encryptionCertificate);
-            cmsEnvelopedDataGenerator.addRecipientInfoGenerator(jceKey);
-            CMSTypedData msg = new CMSProcessableByteArray(data);
-            OutputEncryptor encrypt =
-                    new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC).setProvider(provider).build();
-            CMSEnvelopedData cmsEnvelopedData = cmsEnvelopedDataGenerator.generate(msg, encrypt);
+        LOG.debug("File:" + file +
+                ", X509Certificate:" + encryptionCertificate +
+                ", Provider:" + provider);
 
-            FileOutputStream output = new FileOutputStream(file);
-            output.write(cmsEnvelopedData.getEncoded());
-            output.flush();
-            output.close();
+        try {
+            byte[] data = IOUtils.toByteArray(new FileInputStream(file));
+            if (data != null && encryptionCertificate != null) {
+
+                CMSEnvelopedDataGenerator cmsEnvelopedDataGenerator = new CMSEnvelopedDataGenerator();
+                JceKeyTransRecipientInfoGenerator jceKey = new JceKeyTransRecipientInfoGenerator(encryptionCertificate);
+                cmsEnvelopedDataGenerator.addRecipientInfoGenerator(jceKey);
+                CMSTypedData msg = new CMSProcessableByteArray(data);
+                OutputEncryptor encrypt =
+                        new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC).setProvider(provider).build();
+                CMSEnvelopedData cmsEnvelopedData = cmsEnvelopedDataGenerator.generate(msg, encrypt);
+
+                FileOutputStream output = new FileOutputStream(file);
+                output.write(cmsEnvelopedData.getEncoded());
+                output.flush();
+                output.close();
+
+                LOG.info("File encryption was successful.");
+            }
+        } catch (IOException e) {
+            LOG.error("Error encountered in file handling. " + e.getMessage());
+            throw new Exception("Error encountered in file handling. " + e.getMessage());
+        } catch (CertificateEncodingException e) {
+            LOG.error("Error encountered in certificate handling. " + e.getMessage());
+            throw new Exception("Error encountered in certificate handling. " + e.getMessage());
+        } catch (CMSException e) {
+            LOG.error("Error encountered in encryption handling. " + e.getMessage());
+            throw new Exception("Error encountered in encryption handling. " + e.getMessage());
+        } catch (Exception e) {
+            LOG.error("Unknown error encountered in encryption handling. " + e.getMessage());
+            throw new Exception("Unknown error encountered in encryption handling. " + e.getMessage());
         }
+
+        LOG.info("File encryption failed.");
     }
 
-    public static void decryptFile(File file, PrivateKey decryptionKey) throws CMSException, IOException {
+    public static void decryptFile(File file, PrivateKey decryptionKey) throws Exception {
 
-        byte[] encryptedData = IOUtils.toByteArray(new FileInputStream(file));
-        if (encryptedData != null && decryptionKey != null) {
+        LOG.debug("File:" + file +
+                ", PrivateKey:" + decryptionKey);
 
-            CMSEnvelopedData envelopedData = new CMSEnvelopedData(encryptedData);
-            Collection<RecipientInformation> recipients = envelopedData.getRecipientInfos().getRecipients();
-            KeyTransRecipientInformation recipientInfo = (KeyTransRecipientInformation) recipients.iterator().next();
-            JceKeyTransRecipient recipient = new JceKeyTransEnvelopedRecipient(decryptionKey);
+        try {
+            byte[] encryptedData = IOUtils.toByteArray(new FileInputStream(file));
+            if (encryptedData != null && decryptionKey != null) {
 
-            FileOutputStream output = new FileOutputStream(file);
-            output.write(recipientInfo.getContent(recipient));
-            output.flush();
-            output.close();
+                CMSEnvelopedData envelopedData = new CMSEnvelopedData(encryptedData);
+                Collection<RecipientInformation> recipients = envelopedData.getRecipientInfos().getRecipients();
+                KeyTransRecipientInformation recipientInfo = (KeyTransRecipientInformation) recipients.iterator().next();
+                JceKeyTransRecipient recipient = new JceKeyTransEnvelopedRecipient(decryptionKey);
+
+                FileOutputStream output = new FileOutputStream(file);
+                output.write(recipientInfo.getContent(recipient));
+                output.flush();
+                output.close();
+
+                LOG.info("File decryption was successful.");
+            }
+        } catch (IOException e) {
+            LOG.error("Error encountered in file handling. " + e.getMessage());
+            throw new Exception("Error encountered in file handling. " + e.getMessage());
+        } catch (CMSException e) {
+            LOG.error("Error encountered in decryption handling. " + e.getMessage());
+            throw new Exception("Error encountered in decryption handling. " + e.getMessage());
+        } catch (Exception e) {
+            LOG.error("Unknown error encountered in decryption handling. " + e.getMessage());
+            throw new Exception("Unknown error encountered in decryption handling. " + e.getMessage());
         }
-    }
 
-    public static void main(String[] args) throws Exception {
-
-        ClassLoader classLoader = PgpEncryptDecrypt.class.getClassLoader();
-        Path path = Paths.get(classLoader.getResource("sample.csv").toURI());
-        File file = path.toFile();
-        FileInputStream fis = new FileInputStream(file);
-        String data = IOUtils.toString(fis, "UTF-8");
-        System.out.println("Contents of csv file before encryption");
-        System.out.println("=====================");
-        System.out.println(data);
-        System.out.println("=====================");
-
-        String provider = "BC";
-        Security.addProvider(new BouncyCastleProvider());
-        CertificateFactory certFactory= CertificateFactory.getInstance("X.509", provider);
-        path = Paths.get(classLoader.getResource("sample.cer").toURI());
-        X509Certificate certificate = (X509Certificate) certFactory.generateCertificate(new FileInputStream(path.toFile()));
-        PgpEncryptDecrypt.encryptFile(file, certificate, provider);
-        fis = new FileInputStream(file);
-        data = IOUtils.toString(fis, "UTF-8");
-        System.out.println();
-        System.out.println("Contents of csv file after encryption");
-        System.out.println("*********************");
-        System.out.println(data);
-        System.out.println("*********************");
-
-        char[] keystorePassword = "password".toCharArray();
-        char[] keyPassword = "password".toCharArray();
-        KeyStore keystore = KeyStore.getInstance("PKCS12");
-        path = Paths.get(classLoader.getResource("sample.p12").toURI());
-        keystore.load(new FileInputStream(path.toFile()), keystorePassword);
-        PrivateKey key = (PrivateKey) keystore.getKey("sample", keyPassword);
-        PgpEncryptDecrypt.decryptFile(file, key);
-        fis = new FileInputStream(file);
-        data = IOUtils.toString(fis, "UTF-8");
-        System.out.println();
-        System.out.println("Contents of csv file after decryption");
-        System.out.println("=====================");
-        System.out.println(data);
-        System.out.println("=====================");
+        LOG.info("File decryption was successful.");
     }
 }
---
+++ src/scheduler/src/test/java/org/endeavourhealth/scheduler/util/PgpEncryptDecryptTest.java
@@ -0,0 +1,58 @@
+package org.endeavourhealth.scheduler.util;
+
+import org.apache.commons.io.IOUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class PgpEncryptDecryptTest {
+
+    @Test()
+    public void fullCycle()  {
+
+        try {
+            ClassLoader classLoader = PgpEncryptDecrypt.class.getClassLoader();
+            Path path = Paths.get(classLoader.getResource("sample.csv").toURI());
+            File file = path.toFile();
+            FileInputStream fis = new FileInputStream(file);
+            String originalData = IOUtils.toString(fis, "UTF-8");
+
+            String provider = "BC";
+            Security.addProvider(new BouncyCastleProvider());
+            CertificateFactory certFactory= CertificateFactory.getInstance("X.509", provider);
+            path = Paths.get(classLoader.getResource("sample.cer").toURI());
+            X509Certificate certificate = (X509Certificate) certFactory.generateCertificate(new FileInputStream(path.toFile()));
+            PgpEncryptDecrypt.encryptFile(file, certificate, provider);
+            fis = new FileInputStream(file);
+            String encryptedData = IOUtils.toString(fis, "UTF-8");
+
+            assertNotEquals(originalData, encryptedData);
+
+            char[] keystorePassword = "password".toCharArray();
+            char[] keyPassword = "password".toCharArray();
+            KeyStore keystore = KeyStore.getInstance("PKCS12");
+            path = Paths.get(classLoader.getResource("sample.p12").toURI());
+            keystore.load(new FileInputStream(path.toFile()), keystorePassword);
+            PrivateKey key = (PrivateKey) keystore.getKey("sample", keyPassword);
+            PgpEncryptDecrypt.decryptFile(file, key);
+            fis = new FileInputStream(file);
+            String decyptedData = IOUtils.toString(fis, "UTF-8");
+
+            assertEquals(originalData, decyptedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
