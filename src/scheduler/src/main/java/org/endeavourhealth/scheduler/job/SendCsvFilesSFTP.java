c49e8bccf2bebc9b4840fdc3ac42758ace748bf2
James Biswell <james.biswell@endeavourhealth.org>
2018-11-08 12:35:36 +0000

    Dependency is now to correct endeavourhealth SFTPReader, amended SendCsvFilesSFTP.java to use the correct Classes accordingly.

--- src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/SendCsvFilesSFTP.java
+++ src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/SendCsvFilesSFTP.java
@@ -1,20 +1,13 @@
 package org.endeavourhealth.scheduler.job;
 
-import com.jcraft.jsch.JSchException;
-import org.apache.commons.io.FileUtils;
-import org.endeavourhealth.sftpreader.model.db.DbConfigurationSftp;
-import org.endeavourhealth.sftpreader.utilities.sftp.SftpConnection;
-import org.endeavourhealth.sftpreader.utilities.sftp.SftpConnectionDetails;
-import org.endeavourhealth.sftpreader.utilities.sftp.SftpConnectionException;
+import org.endeavourhealth.sftpreader.sources.ConnectionDetails;
+import org.endeavourhealth.sftpreader.sources.Connection;
+import org.endeavourhealth.sftpreader.sources.SftpConnection;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.*;
-import java.net.URI;
-import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SendCsvFilesSFTP implements Job {
 
@@ -22,11 +15,11 @@ public class SendCsvFilesSFTP implements Job {
 
     private final String readFile(String file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader (file));
-        String         line = null;
-        StringBuilder  stringBuilder = new StringBuilder();
-        String         ls = System.getProperty("line.separator");
+        String line = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String ls = System.getProperty("line.separator");
         try {
-            while((line = reader.readLine()) != null) {
+                while((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
                 stringBuilder.append(ls);
             }
@@ -58,29 +51,30 @@ public class SendCsvFilesSFTP implements Job {
         catch (Exception e) { System.out.println("Exception occurred while reading clientPrivateKey file. " + e);}
         // System.out.println(clientPrivateKey);
 
-        // String clientPrivateKeyPassword = null;
-        // String hostPublicKey = null;
+        String clientPrivateKeyPassword = "";
+        String hostPublicKey = "";
 
-        SftpConnectionDetails sftpconnectiondetails;
-        SftpConnection sftpconnection;
+        ConnectionDetails sftpConnectionDetails;
+        Connection sftpConnection;
 
-        sftpconnectiondetails = new SftpConnectionDetails();
-        sftpconnectiondetails.setHostname(hostname);
-        sftpconnectiondetails.setPort(port);
-        sftpconnectiondetails.setUsername(username);
-        sftpconnectiondetails.setClientPrivateKey(clientPrivateKey);
-        // sftpconnectiondetails.setClientPrivateKeyPassword(clientPrivateKeyPassword);
-        // sftpconnectiondetails.setHostPublicKey(hostPublicKey);
+        sftpConnectionDetails = new ConnectionDetails();
+        sftpConnectionDetails.setHostname(hostname);
+        sftpConnectionDetails.setPort(port);
+        sftpConnectionDetails.setUsername(username);
+        sftpConnectionDetails.setClientPrivateKey(clientPrivateKey);
+        sftpConnectionDetails.setClientPrivateKeyPassword(clientPrivateKeyPassword);
+        sftpConnectionDetails.setHostPublicKey(hostPublicKey);
 
-        sftpconnection = new SftpConnection(sftpconnectiondetails);
+        sftpConnection = new SftpConnection(sftpConnectionDetails);
 
         try {
             // Try opening the connection to the SFTP and writing to it
-            sftpconnection.open();
+            sftpConnection.open();
+            System.out.println("Connected to the SFTP");
             // String localPath = "C:/sftpupload";
-            // String destinationPath = sftpconnectiondetails.getKnownHostsString();
-            // sftpconnection.put(localPath, destinationPath);
-            // sftpconnection.getFile(destinationPath);
+            // String destinationPath = sftpConnectionDetails.getKnownHostsString();
+            // sftpConnection.put(localPath, destinationPath);
+            // sftpConnection.getFile(destinationPath);
         }
         catch (Exception e) {
             // Catch if there is a problem connecting to it
@@ -89,42 +83,9 @@ public class SendCsvFilesSFTP implements Job {
         }
         finally {
             // Close the connection to the SFTP
-            if (sftpconnection != null)
-                sftpconnection.close();
-        }
-
-        /* Helper methods from SftpReaderTask
-
-        private static SftpConnection openSftpConnection (DbConfigurationSftp configurationSftp) throws SftpConnectionException, JSchException, IOException {
-           SftpConnection sftpConnection = new SftpConnection(getSftpConnectionDetails(configurationSftp));
-
-            String hostname = sftpConnection.getConnectionDetails().getHostname();
-            String port = Integer.toString(sftpConnection.getConnectionDetails().getPort());
-            String username = sftpConnection.getConnectionDetails().getUsername();
-
-            LOG.info(" Opening SFTP connection to " + hostname + " on port " + port + " with user " + username);
-
-            sftpConnection.open();
-
-            return sftpConnection;
-        }
-
-        private static SftpConnectionDetails getSftpConnectionDetails(DbConfigurationSftp configurationSftp) {
-            return new SftpConnectionDetails()
-                    .setHostname(configurationSftp.getHostname())
-                    .setPort(configurationSftp.getPort())
-                    .setUsername(configurationSftp.getUsername())
-                    .setClientPrivateKey(configurationSftp.getClientPrivateKey())
-                    .setClientPrivateKeyPassword(configurationSftp.getClientPrivateKeyPassword())
-                    .setHostPublicKey(configurationSftp.getHostPublicKey());
-        }
-
-        private static void closeConnection(SftpConnection sftpConnection) {
             if (sftpConnection != null)
                 sftpConnection.close();
         }
-
-        */
 
         /* try {
             // Don't know where the files will be after encryption, or where
@@ -157,6 +118,6 @@ public class SendCsvFilesSFTP implements Job {
             System.out.println("I/O error occurred" + " " + e);
         }
         */
-        // System.out.println("CSV files sent");
+        System.out.println("CSV files sent");
     }
 }
