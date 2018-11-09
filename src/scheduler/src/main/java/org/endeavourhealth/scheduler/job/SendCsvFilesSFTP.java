1ab13e8c46daeae5eb6f97349e7ea33ad7e9775c
James Biswell <james.biswell@endeavourhealth.org>
2018-11-09 17:47:02 +0000

    SendCsvFilesSFTP.java opens SFTP connection, reads its directory, gets a CSV file from SFTP and puts it on C drive, and gets a CSV file from C drive and puts it on SFTP

--- src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/SendCsvFilesSFTP.java
+++ src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/SendCsvFilesSFTP.java
@@ -61,8 +61,8 @@ public class SendCsvFilesSFTP implements Job {
 
             // Opening a connection to the SFTP
             sftpConnection = ConnectionActivator.createConnection(sftpConnectionDetails);
-            System.out.println("Opening " + sftpConnection.getClass().getName() + " to " + hostname +
-                    " on port " + port + " with user " + username);
+            System.out.println("Opening " + sftpConnection.getClass().getName() + " to "
+                    + hostname + " on port " + port + " with user " + username);
             sftpConnection.open();
             System.out.println("Connected to the SFTP");
 
@@ -72,61 +72,36 @@ public class SendCsvFilesSFTP implements Job {
             System.out.println(remoteFileList);
 
             // Getting a file from the SFTP directory ...
-            InputStream inputStream = sftpConnection.getFile("/endeavour/ftp/test1onsftp.csv");
+            InputStream downloadInputStream = sftpConnection.getFile("/endeavour/ftp/test1onsftp.csv");
             System.out.println("Getting file from SFTP");
 
             // ... and putting it onto the C drive
-            File downloadDestination = new File("C:/sftpdownload/test1fromsftp.csv");
-            Files.copy(inputStream, downloadDestination.toPath());
+            String downloadDestinationString = "C:/sftpdownload/test1fromsftp.csv";
+            File downloadDestination = new File(downloadDestinationString);
+            Files.copy(downloadInputStream, downloadDestination.toPath());
             System.out.println("Downloading file to: " + downloadDestination);
 
             // Getting a file from the C drive and putting it onto the SFTP directory
-            String localPath = "C:/sftpupload/test1oncdrive.csv";
-            String destinationPath = "/endeavour/ftp/test1fromcdrive.csv"; // remote path
-            // sftpConnection.put(localPath, destinationPath);
+
+            // InputStream uploadInputStream = new FileInputStream("C:/sftpupload/test1oncdrive.csv");
+            // System.out.println("Getting file from C drive");
+            String sourceLocalPath = "C:/sftpupload/test1oncdrive.csv";
+            String uploadDestinationString = "/endeavour/ftp/test1fromcdrive.csv"; // remote path
+            sftpConnection.put(sourceLocalPath, uploadDestinationString);
+            System.out.println("Uploading file to: " + uploadDestinationString);
         }
+
         catch (Exception e) {
             // Catch if there is a problem while connecting to, or using, the SFTP
             // LOG.error("Exception occurred with using the SFTP." + e);
             System.out.println("Exception occurred with using the SFTP. " + e);
         }
+
         finally {
             // Close the connection to the SFTP
             if (sftpConnection != null)
                 sftpConnection.close();
         }
-
-        /* try {
-            // Don't know where the files will be after encryption, or where
-            // they are going to, so will need to use URIs rather hard-coded paths
-            // URI sourceURI = URI.create("C:/source");
-            // URI targetURI = URI.create("C:/target");
-
-            // This takes the source directory folder and recreates it and
-            // its contents in the target location, overwriting everything
-            String source = "C:/source";
-            File sourceDir = new File(source);
-            String target = "C:/target";
-            File targetDir = new File(target);
-            FileUtils.copyDirectory(sourceDir, targetDir);
-
-            // This just takes the source directory folder and only creates the target
-            // folder if none is already there, otherwise there's an exception
-            // Path sourcePath = Paths.get("C:/source");
-            // Path targetPath = Paths.get("C:/target");
-            // Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
-
-            // Still thinking about this - want to get a collection of files from a specific
-            // directory, which can then be iteratively uploaded to an SFTP, or can you just
-            // upload a directory all in one hit?
-            // List<File> files = new ArrayList<>();
-            // files.add(Files.walkFileTree(sourcePath, new SimpleFileVisitor<>())); - doesn't work
-            //
-            System.out.println("CSV files sent");
-        } catch (IOException e) {
-            System.out.println("I/O error occurred" + " " + e);
-        }
-        */
         System.out.println("CSV files sent");
     }
 
