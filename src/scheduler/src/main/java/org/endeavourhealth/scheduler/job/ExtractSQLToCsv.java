c821232446f22243b3577a7801c96d3998b30a11
Froiland Go <Froiland.Go@learning-health.co.uk>
2018-11-14 16:46:03 +0000

    Streaming table contents

--- src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/ExtractSQLToCsv.java
+++ src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/ExtractSQLToCsv.java
@@ -3,16 +3,28 @@ package org.endeavourhealth.scheduler.job;
 import org.endeavourhealth.scheduler.cache.DatasetCache;
 import org.endeavourhealth.scheduler.json.DatasetConfig;
 import org.endeavourhealth.scheduler.json.DatasetConfigExtract;
+import org.endeavourhealth.scheduler.models.PersistenceJPAConfig;
 import org.endeavourhealth.scheduler.models.PersistenceManager;
+import org.endeavourhealth.scheduler.models.database.DatasetEntity;
+import org.endeavourhealth.scheduler.models.repository.DatasetEntityRepository;
+import org.hibernate.Session;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
-import java.io.FileWriter;
+import java.io.*;
+import java.sql.Connection;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class ExtractSQLToCsv implements Job {
 
@@ -147,5 +159,73 @@ public class ExtractSQLToCsv implements Job {
             fw.close();
             System.out.println("Headers saved in " + tableName + ".csv");
         }
+    }
+
+
+    public static void main(String args[]) {
+        ExtractSQLToCsv e = new ExtractSQLToCsv();
+        e.test();
+    }
+
+    @Transactional(readOnly = true)
+    public void test() {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(PersistenceJPAConfig.class);
+        LocalContainerEntityManagerFactoryBean cefb = ctx.getBean(LocalContainerEntityManagerFactoryBean.class);
+        EntityManagerFactory emf = cefb.getObject();
+        EntityManager entityManager = emf.createEntityManager();
+
+        Query query = entityManager.createNativeQuery("select * from dataset");
+        List<Object[]> results = query.getResultList();
+        System.out.println("size:"+results.size());
+
+        DatasetEntityRepository datasetEntityRepo = ctx.getBean(DatasetEntityRepository.class);
+        streamedCsv(new File("C:\\Temp\\test.csv"), datasetEntityRepo, entityManager);
+
+    }
+
+    @Transactional(readOnly = true)
+    public void streamedCsv(File file, DatasetEntityRepository datasetEntityRepo, EntityManager entityManager ) {
+        Connection conn = entityManager.unwrap(Session.class).connection();
+
+
+        try(Stream<DatasetEntity> datasetEntityStream = datasetEntityRepo.streamAll()) {
+            PrintWriter out = new PrintWriter(new FileOutputStream(file));
+            datasetEntityStream.forEach(rethrowConsumer(datasetEntity -> {
+                String line = formLine(datasetEntity);
+                out.write(line);
+                out.write("\n");
+                entityManager.detach(datasetEntity);
+            }));
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            LOG.info("Exception occurred " + e.getMessage(), e);
+            throw new RuntimeException("Exception occurred while exporting results", e);
+        }
+    }
+
+
+    private static String formLine(DatasetEntity entity) {
+        return String.join(entity.getDatasetId() +  "," + entity.getDefinition());
+    }
+
+    private static <T, E extends Exception> Consumer<T> rethrowConsumer(Consumer_WithExceptions<T, E> consumer) throws E {
+        return t -> {
+            try {
+                consumer.accept(t);
+            } catch (Exception exception) {
+                throwActualException(exception);
+            }
+        };
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <E extends Exception> void throwActualException(Exception exception) throws E {
+        throw (E) exception;
+    }
+
+    @FunctionalInterface
+    private interface Consumer_WithExceptions<T, E extends Exception> {
+        void accept(T t) throws E;
     }
 }
