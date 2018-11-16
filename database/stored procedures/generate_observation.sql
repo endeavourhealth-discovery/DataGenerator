2f31cdb24590dd477b7ee2184757233d7f635adc
stuart <stuart@bearclawsolutions.co.uk>
2018-11-16 16:39:09 +0000

    First attempt at getting only the latest or earliest of a code in a code set.  Can handle mixtures in there too.

--- database/stored procedures/generate_observation.sql
+++ database/stored procedures/generate_observation.sql
@@ -30,19 +30,100 @@ CREATE PROCEDURE generate_observation_all_col (
 )
 BEGIN
 
+  -- get the latest of each code in the dataset for the patient for codes defined as latest only
+  create table latest_codes as
+  select distinct o.id -- min(o.effective_date) as effective_date, o.original_code, o.patient_id
+  from pcr.observation o 
+  left join pcr.observation oo on oo.patient_id = o.patient_id
+								and oo.original_code = o.original_code
+                                and (o.effective_date < oo.effective_date
+									or (o.effective_date = oo.effective_date and o.id < oo.id))
+  join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
+  join subscriber_transform.code_set_codes csc on csc.read2_concept_id = o.original_code 
+												and csc.code_set_id = codeSetId
+                                                and csc.extract_type = 2
+  where oo.patient_id is null;
+                                                
+   /*                                             
+  select max(o.effective_date) as effective_date, o.original_code, o.patient_id
+  from pcr.observation o 
+  join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
+  join subscriber_transform.code_set_codes csc on csc.read2_concept_id = o.original_code 
+												and csc.code_set_id = codeSetId
+                                                and csc.extract_type = 2
+   group by o.patient_id, o.original_code;
+  
+  */
+  -- get the earliest of each code in the dataset for the patient for codes defined as earliest only
+  create table earliest_codes as
+  select distinct o.id -- min(o.effective_date) as effective_date, o.original_code, o.patient_id
+  from pcr.observation o 
+  left join pcr.observation oo on oo.patient_id = o.patient_id
+								and oo.original_code = o.original_code
+                                and (o.effective_date > oo.effective_date
+									or (o.effective_date = oo.effective_date and o.id < oo.id))
+  join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
+  join subscriber_transform.code_set_codes csc on csc.read2_concept_id = o.original_code 
+												and csc.code_set_id = codeSetId
+                                                and csc.extract_type = 1
+  where oo.patient_id is null;
+  
+  /*
+  select min(o.effective_date) as effective_date, o.original_code, o.patient_id
+  from pcr.observation o 
+  join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
+  join subscriber_transform.code_set_codes csc on csc.read2_concept_id = o.original_code 
+												and csc.code_set_id = codeSetId
+                                                and csc.extract_type = 1
+							
+   group by o.patient_id, o.original_code;
+   */
   CREATE TABLE obs_test as 
   SELECT o.* FROM pcr.observation o
   join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
-  join subscriber_transform.code_set_codes csc on csc.read2_concept_id = o.original_code and csc.code_set_id = codeSetId
+  join subscriber_transform.code_set_codes csc on csc.read2_concept_id = o.original_code 
+												and csc.code_set_id = codeSetId
+                                                and csc.extract_type = 0 -- all codes
+  where cr.bulked = 0
+  union
+  SELECT o.* FROM pcr.observation o
+  join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
+  join earliest_codes ec on o.patient_id = ec.id  -- earliest codes only
+  where cr.bulked = 0
+  union  
+  SELECT o.* FROM pcr.observation o
+  join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
+  join latest_codes ec on o.id = ec.id  -- latest codes only
   where cr.bulked = 0
   union
   SELECT o.* FROM pcr.observation o
   join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
   join data_generator.extract ex on ex.extract_id = cr.extract_id
-  join subscriber_transform.code_set_codes csc on csc.read2_concept_id = o.original_code and csc.code_set_id = codeSetId
+  join subscriber_transform.code_set_codes csc on csc.read2_concept_id = o.original_code 
+												and csc.code_set_id = codeSetId
+                                                and csc.extract_type = 0 -- all codes                                                
   join pcr.event_log e on e.item_id = o.id and e.table_name = 'Observation'
   where cr.bulked = 1
+    and e.id > ex.transaction_id and e.id <= maxTransactionId
+  union
+  SELECT o.* FROM pcr.observation o
+  join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
+  join data_generator.extract ex on ex.extract_id = cr.extract_id
+  join earliest_codes ec on o.id = ec.id  -- earliest codes only  
+  join pcr.event_log e on e.item_id = ec.id and e.table_name = 'Observation'
+  where cr.bulked = 1
+    and e.id > ex.transaction_id and e.id <= maxTransactionId
+  union
+  SELECT o.* FROM pcr.observation o
+  join data_generator.cohort_results cr on cr.patient_id = o.patient_id and cr.extract_id = extractId
+  join data_generator.extract ex on ex.extract_id = cr.extract_id
+  join latest_codes ec on o.id = ec.id  -- latest codes only  
+  join pcr.event_log e on e.item_id = ec.id and e.table_name = 'Observation'
+  where cr.bulked = 1
     and e.id > ex.transaction_id and e.id <= maxTransactionId;
+    
+  drop table if exists earliest_codes;
+  drop table if exists latest_codes;
       
 END//
 DELIMITER ;
