aed5ea247d9bdc256c16008abf12b6f53bd8808d
Froiland Go <Froiland.Go@learning-health.co.uk>
2018-11-26 10:57:55 +0000

    Modified folder structure

--- database/schema/populate tables script.sql
+++ database/schema/populate tables script.sql
@@ -31,8 +31,7 @@ select 1, '{
 }';
 
 -- delete some extract data
-delete from data_generator.extract where extract_id = 1;
-delete from data_generator.extract where extract_id = 2;
+delete from data_generator.extract where extract_id <= 4;
 
 -- create some extract data for extract 1
 insert into data_generator.extract
@@ -40,9 +39,9 @@ select 1, 'Child Imms', 1, 1, 1, '{
 "name": "Data Generator Extract Definition 1",
  "id": "1",
  "fileLocationDetails": {
-          "source": "C:/extract1testsource",
-          "destination": "/endeavour/ftp/",
-          "housekeep": "C:/extract1testhousekeep"
+          "source": "C:\\DataGenerator\\SubscriberA\\Source\\",
+          "destination": "/endeavour/ftp/SubscriberA/",
+          "housekeep": "C:\\DataGenerator\\SubscriberA\\Housekeep\\"
 },
  "sftpConnectionDetails": {
           "hostname": "10.0.101.239",
@@ -60,9 +59,49 @@ select 2, 'Health check', 1, 1, 1, '{
 "name": "Data Generator Extract Definition 2",
  "id": "2",
  "fileLocationDetails": {
-          "source": "C:/extract2testsource",
-          "destination": "/endeavour/ftp/",
-          "housekeep": "C:/extract2testhousekeep"
+          "source": "C:\\DataGenerator\\SubscriberA\\Source\\",
+          "destination": "/endeavour/ftp/SubscriberA/",
+          "housekeep": "C:\\DataGenerator\\SubscriberA\\Housekeep\\"
+},
+ "sftpConnectionDetails": {
+          "hostname": "10.0.101.239",
+          "hostPublicKey": "",
+          "port": "22",
+          "username": "endeavour",
+          "clientPrivateKeyPassword": "",
+          "clientPrivateKey": "PuTTY-User-Key-File-2: ssh-rsa\\r\\nEncryption: none\\r\\nComment: imported-openssh-key\\r\\nPublic-Lines: 6\\r\\nAAAAB3NzaC1yc2EAAAADAQABAAABAQDNb7bfn3hEjjUtnkHCgIeVzUJKRXidXoYP\\r\\na0pzIVEv9Dgo6zN4ajUE2mCCgFi2Y16yJVupRIkFkZ8d31VItkMbrZIyV0wCekDs\\r\\n0I6gxh4V5XJrsiWTHfk4fAajb2AWcjt3Id9Hray8zPoiwE0RxSNcAGHNpR9OO82S\\r\\nndotGj2bOzBXN+16lEWF/bi2ZQ77dYiYKKuzvyyD8MMXJ2E5BTewli4SW8KRxQIl\\r\\nMt1tRfRexUfjDIIn6cyOMs9WCCsr5GC2hQuyyAGNQw2pAxvUvlOYl+WP1h8IPcrS\\r\\nR7lGtmXWfl4hc8zfmR1/jafIkk5SK/W9KpYBHLLQA5EQFZezVQeL\\r\\nPrivate-Lines: 14\\r\\nAAABAFH0rE8ADnnDBcICLZfLsMt5TGXW2yxkxjSmh9fwRbRMyI7CbhEuxaH/AJtv\\r\\nWnTApcmKD8wyVDuNgZ3oN9y/IXyMPROqMd+XKAmRliTbhKsVkxUVx9muDnuTNw7C\\r\\nYuHxhnmbYLj2tz/Gwk9Uyio5rEaKvHnO0vNh9jv0j1KI8mTXevl5Eun9JFpf6bgl\\r\\nRhoobNhBtJSw4vmz352FFO2vTtaqti/idU6AVeB2XWhYDFntaPUfrFmoPMI8VUte\\r\\nhBdPIP1ty/RZQBjaccdugNJmGR08D68LObfOvpr2FH9xRKVh4KIzmaU84n1n011A\\r\\nWd3Vda3m5O6J2DsvLZWsVsGLkkkAAACBAP/aS7s8xPg6GAK7vf9c0f/Z5jYBmoWQ\\r\\nXVLdTbUvuuElgmCvpy6/tPt+fFgFqkOW64fl3RDMM6R1afSoXGH9wtCkG+zTeJam\\r\\n1HtSGQ205qTG2l5TZzIsDqgX75d0+ObIpoHorUmpRBIKz0i5VQrw0EqoTxo18P+h\\r\\nCYsSr0ccBDvtAAAAgQDNjf0kQi0fBr8uIW/dF1CAdnFpbFWsAgecqnxEHHC5wGCM\\r\\nw+tZdpzxNa/Qr1stUdDZvs6XCGs8yB70tQ5Dxac0ZeK6VM6JcF60AQomPe7GsBXH\\r\\nwGUWSa2v9szkTb4fQcrCTYL7U6SVixq8vAiVYfjExV0smuuOybzjIiXpXqYSVwAA\\r\\nAIEAiU0ecoZ+2eN6Lf9d46jxKbYmj6J675UFIxIeYaPIz4Ev1aznXwsHPg9oog3X\\r\\nlGkIJavmZAgabuiK8yjVwUtCvs1dC0KvRIDhEkVEG6y5c/ExLTf9/uWetlhVdFZQ\\r\\nK13Fln7c/PPILty6YJ7etxW7c2+QhID27IeV5GbwvEjxcpk=\\r\\nPrivate-MAC: a58be360183ea42a152010bb51b38f4277f6871e"
+}
+}',1;
+
+-- create some extract data for extract 3
+insert into data_generator.extract
+select 3, 'Child Imms', 1, 1, 1, '{
+"name": "Data Generator Extract Definition 3",
+ "id": "3",
+ "fileLocationDetails": {
+          "source": "C:\\DataGenerator\\SubscriberB\\Source\\",
+          "destination": "/endeavour/ftp/SubscriberB/",
+          "housekeep": "C:\\DataGenerator\\SubscriberB\\Housekeep\\"
+},
+ "sftpConnectionDetails": {
+          "hostname": "10.0.101.239",
+          "hostPublicKey": "",
+          "port": "22",
+          "username": "endeavour",
+          "clientPrivateKeyPassword": "",
+          "clientPrivateKey": "PuTTY-User-Key-File-2: ssh-rsa\\r\\nEncryption: none\\r\\nComment: imported-openssh-key\\r\\nPublic-Lines: 6\\r\\nAAAAB3NzaC1yc2EAAAADAQABAAABAQDNb7bfn3hEjjUtnkHCgIeVzUJKRXidXoYP\\r\\na0pzIVEv9Dgo6zN4ajUE2mCCgFi2Y16yJVupRIkFkZ8d31VItkMbrZIyV0wCekDs\\r\\n0I6gxh4V5XJrsiWTHfk4fAajb2AWcjt3Id9Hray8zPoiwE0RxSNcAGHNpR9OO82S\\r\\nndotGj2bOzBXN+16lEWF/bi2ZQ77dYiYKKuzvyyD8MMXJ2E5BTewli4SW8KRxQIl\\r\\nMt1tRfRexUfjDIIn6cyOMs9WCCsr5GC2hQuyyAGNQw2pAxvUvlOYl+WP1h8IPcrS\\r\\nR7lGtmXWfl4hc8zfmR1/jafIkk5SK/W9KpYBHLLQA5EQFZezVQeL\\r\\nPrivate-Lines: 14\\r\\nAAABAFH0rE8ADnnDBcICLZfLsMt5TGXW2yxkxjSmh9fwRbRMyI7CbhEuxaH/AJtv\\r\\nWnTApcmKD8wyVDuNgZ3oN9y/IXyMPROqMd+XKAmRliTbhKsVkxUVx9muDnuTNw7C\\r\\nYuHxhnmbYLj2tz/Gwk9Uyio5rEaKvHnO0vNh9jv0j1KI8mTXevl5Eun9JFpf6bgl\\r\\nRhoobNhBtJSw4vmz352FFO2vTtaqti/idU6AVeB2XWhYDFntaPUfrFmoPMI8VUte\\r\\nhBdPIP1ty/RZQBjaccdugNJmGR08D68LObfOvpr2FH9xRKVh4KIzmaU84n1n011A\\r\\nWd3Vda3m5O6J2DsvLZWsVsGLkkkAAACBAP/aS7s8xPg6GAK7vf9c0f/Z5jYBmoWQ\\r\\nXVLdTbUvuuElgmCvpy6/tPt+fFgFqkOW64fl3RDMM6R1afSoXGH9wtCkG+zTeJam\\r\\n1HtSGQ205qTG2l5TZzIsDqgX75d0+ObIpoHorUmpRBIKz0i5VQrw0EqoTxo18P+h\\r\\nCYsSr0ccBDvtAAAAgQDNjf0kQi0fBr8uIW/dF1CAdnFpbFWsAgecqnxEHHC5wGCM\\r\\nw+tZdpzxNa/Qr1stUdDZvs6XCGs8yB70tQ5Dxac0ZeK6VM6JcF60AQomPe7GsBXH\\r\\nwGUWSa2v9szkTb4fQcrCTYL7U6SVixq8vAiVYfjExV0smuuOybzjIiXpXqYSVwAA\\r\\nAIEAiU0ecoZ+2eN6Lf9d46jxKbYmj6J675UFIxIeYaPIz4Ev1aznXwsHPg9oog3X\\r\\nlGkIJavmZAgabuiK8yjVwUtCvs1dC0KvRIDhEkVEG6y5c/ExLTf9/uWetlhVdFZQ\\r\\nK13Fln7c/PPILty6YJ7etxW7c2+QhID27IeV5GbwvEjxcpk=\\r\\nPrivate-MAC: a58be360183ea42a152010bb51b38f4277f6871e"
+}
+}',1;
+
+-- create some extract data for extract 4
+insert into data_generator.extract
+select 4, 'Health check', 1, 1, 1, '{
+"name": "Data Generator Extract Definition 4",
+ "id": "4",
+ "fileLocationDetails": {
+          "source": "C:\\DataGenerator\\SubscriberB\\Source\\",
+          "destination": "/endeavour/ftp/SubscriberB/",
+          "housekeep": "C:\\DataGenerator\\SubscriberB\\Housekeep\\"
 },
  "sftpConnectionDetails": {
           "hostname": "10.0.101.239",
