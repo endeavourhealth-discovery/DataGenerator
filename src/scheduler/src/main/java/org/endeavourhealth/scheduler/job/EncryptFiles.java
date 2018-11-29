17b2f9598854f93e44e4feb12189f09752a3b2f8
Froiland Go <Froiland.Go@learning-health.co.uk>
2018-11-29 18:19:49 +0000

    Removed needless imports

--- src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/EncryptFiles.java
+++ src/scheduler/src/main/java/org/endeavourhealth/scheduler/job/EncryptFiles.java
@@ -13,11 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.NoSuchProviderException;
 import java.security.Security;
 import java.security.cert.CertificateException;
