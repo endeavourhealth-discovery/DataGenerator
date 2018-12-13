5e8f5230819c498dd509252495b54323ccc99c82
stuart <stuart@bearclawsolutions.co.uk>
2018-12-13 09:44:27 +0000

    Update patient extracts to include organisation details

--- src/scheduler/src/main/java/org/endeavourhealth/scheduler/models/CustomExtracts/PatientExtracts.java
+++ src/scheduler/src/main/java/org/endeavourhealth/scheduler/models/CustomExtracts/PatientExtracts.java
@@ -43,11 +43,14 @@ public class PatientExtracts {
                     " a.address_line_4," +
                     " a.postcode," +
                     " a.uprn," +
-                    " a.approximation_concept_id," +
-                    " a.property_type_concept_id" +
+                    " a.approximation_concept_id, " +
+                    " a.property_type_concept_id, " +
+                    " org.ods_code, " +
+                    " org.name as organisation_name " +
                     " FROM pcr2.patient p " +
                     " left outer join pcr2.patient_address pa on pa.address_id = p.home_address_id " +
                     " left outer join pcr2.address a on a.id = pa.address_id " +
+                    " left outer join pcr2.organisation org on org.id = p.organisation_id " +
                     " join data_generator.cohort_results cr on cr.patient_id = p.id and cr.extract_id = :extractId " +
                     " where cr.bulked = 0;";
             Query query = entityManager.createNativeQuery(sql)
@@ -68,7 +71,39 @@ public class PatientExtracts {
         EntityManager entityManager = PersistenceManager.getEntityManager();
 
         try {
-            String sql = "SELECT p.* FROM pcr2.patient p " +
+            String sql = "SELECT " +
+                    " p.id," +
+                    " p.organisation_id," +
+                    " p.nhs_number," +
+                    " p.nhs_number_verification_concept_id," +
+                    " p.date_of_birth," +
+                    " p.date_of_death," +
+                    " p.gender_concept_id," +
+                    " p.usual_practitioner_id," +
+                    " p.care_provider_id," +
+                    " p.entered_by_practitioner_id," +
+                    " p.title," +
+                    " p.first_name," +
+                    " p.middle_names," +
+                    " p.last_name," +
+                    " p.previous_last_name," +
+                    " p.home_address_id," +
+                    " p.is_spine_sensitive," +
+                    // " p.ethnic_code," +
+                    " a.address_line_1," +
+                    " a.address_line_2," +
+                    " a.address_line_3," +
+                    " a.address_line_4," +
+                    " a.postcode," +
+                    " a.uprn," +
+                    " a.approximation_concept_id, " +
+                    " a.property_type_concept_id, " +
+                    " org.ods_code, " +
+                    " org.name as organisation_name " +
+                    " FROM pcr2.patient p " +
+                    " left outer join pcr2.patient_address pa on pa.address_id = p.home_address_id " +
+                    " left outer join pcr2.address a on a.id = pa.address_id " +
+                    " left outer join pcr2.organisation org on org.id = p.organisation_id " +
                     " join data_generator.cohort_results cr on cr.patient_id = p.id and cr.extract_id = :extractId " +
                     " join (select item_id from pcr2.event_log e " +
                     "       where e.table_id = 8 " +
