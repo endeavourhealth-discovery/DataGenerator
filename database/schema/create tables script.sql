5527836480347c7a9d152211029bd4ffb54dbc7c
Froiland Go <Froiland.Go@learning-health.co.uk>
2019-02-04 09:23:06 +0000

    Added cron column in extract

--- database/schema/create tables script.sql
+++ database/schema/create tables script.sql
@@ -24,6 +24,7 @@ create table data_generator.extract (
     dataset_id int not null comment 'The dataset to be used for this extract',
     definition varchar(5000) not null comment 'The json definition of the extract',
     transaction_id bigint not null comment 'The latest transaction id that was extracted',
+    cron varchar(30) null comment 'The cron timing of the extract',
     
     constraint data_generator_extract_id_pk primary key (extract_id),
     foreign key data_generator_extract_dataset_id_fk (dataset_id) references data_generator.dataset(dataset_id)    
