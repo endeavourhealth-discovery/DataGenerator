f17ff049561c50bb3ec0a2888ddc95f0e27b1ea4
JamesBiswellEnd <james.biswell@endeavourhealth.org>
2019-05-29 13:09:59 +0100

    Added first draft of SFTP upload to cegdatabasefilesender module.

--- src/cegdatabasefilesender/src/main/java/org/endeavourhealth.cegdatabasefilesender/Main.java
+++ src/cegdatabasefilesender/src/main/java/org/endeavourhealth.cegdatabasefilesender/Main.java
@@ -5,6 +5,11 @@ import org.endeavourhealth.core.database.dal.DalProvider;
 import org.endeavourhealth.core.database.dal.audit.QueuedMessageDalI;
 // import org.endeavourhealth.core.database.rdbms.audit.models.RdbmsQueuedMessage;
 import org.endeavourhealth.core.database.rdbms.ConnectionManager;
+import org.endeavourhealth.scheduler.job.EncryptFiles;
+// import org.endeavourhealth.scheduler.json.ExtractDefinition.ExtractConfig;
+import org.endeavourhealth.scheduler.util.ConnectionDetails;
+import org.endeavourhealth.scheduler.util.PgpEncryptDecrypt;
+import org.endeavourhealth.scheduler.util.SftpConnection;
 import org.hibernate.internal.SessionImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,14 +32,12 @@ import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.util.Zip4jConstants;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.endeavourhealth.scheduler.job.EncryptFiles;
-import org.endeavourhealth.scheduler.util.PgpEncryptDecrypt;
 import org.apache.commons.io.FileUtils;
 
 public class Main {
 
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
-    private static Main instance = null;
+    // private static Main instance = null;
 
     /* private Main() {
     } */
@@ -51,8 +54,12 @@ public class Main {
         // ConfigManager.Initialize("ceg-database-file-sender");
         // Main main = Main.getInstance();
 
+        // TODO Amend pathnames as required
         File dataDir = new File("C:/JDBC/Data/");
         File stagingDir = new File("C:/JDBC/Staging/");
+
+        LOG.info("**********");
+        LOG.info("Starting Process.");
 
         LOG.info("**********");
         LOG.info("Getting stored zipped CSV files from audit.queued_message table, to write to data directory.");
@@ -116,7 +123,6 @@ public class Main {
 
         // LOG.info("**********");
         // LOG.info("Deleting contents of data directory.");
-
         // TODO Put code here
 
         LOG.info("**********");
@@ -141,10 +147,54 @@ public class Main {
         LOG.info("**********");
         LOG.info("Checking staging directory to send contents to CEG SFTP location.");
 
+        try {
+            ConnectionDetails con = setSubscriberConfigSftpConnectionDetails();
+            SftpConnection sftp = new SftpConnection(con);
+
+            try {
+                sftp.open();
+                // LOG.info("**********");
+                // LOG.info("SFTP connection opened.");
+
+                try {
+                    // TODO Amend SFTP location as required
+                    String location = "/endeavour/ftp/Test/";
+                    File[] files = stagingDir.listFiles();
+                    // LOG.info("**********");
+                    // LOG.info("Starting file/s upload.");
+                    for (File file : files) {
+                        LOG.info("**********");
+                        LOG.info("Uploading file: " + file.getName());
+                        sftp.put(file.getAbsolutePath(), location);
+                    }
+                    sftp.close();
+
+                } catch (Exception ex) {
+                    LOG.info("**********");
+                    LOG.error("Error encountered while uploading to the SFTP: " + ex.getMessage());
+                    System.exit(-1);
+                }
+
+            } catch (Exception ex) {
+                LOG.info("**********");
+                LOG.error("Error encountered while connecting to the SFTP: " + ex.getMessage());
+                System.exit(-1);
+            }
+
+        } catch (Exception ex) {
+            LOG.info("**********");
+            LOG.error("Exception occurred while reading clientPrivateKey file. " + ex);
+            System.exit(-1);
+        }
+
+        // LOG.info("**********");
+        // LOG.info("Archiving contents of staging directory.");
         // TODO Put code here
+        File archiveDir = new File("C:/JDBC/Archive/");
 
-
-
+        LOG.info("**********");
+        LOG.info("Process Completed.");
+        System.exit(0);
 
     }
 
@@ -187,6 +237,7 @@ public class Main {
         QueuedMessageDalI queuedMessageDal = DalProvider.factoryQueuedMessageDal();
         try {
             queuedMessageDal.delete(queuedMessageId);
+
         } catch (Exception ex) {
             throw ex;
         }
@@ -200,8 +251,9 @@ public class Main {
                 queuedMessageId.toString() +
                 "_Subscriber_Zip.zip");
 
-        // Full UUID needs to be used in the filename because several different files can be written in the same
-        // millisecond so using the code below to attempt filename uniqueness causes files to be overwritten, when looped
+        // Full UUID needs to be used in the filename because several different files can be
+        // written in the same millisecond so using the code below to attempt filename
+        // uniqueness causes files to be overwritten, when this method is in a loop
            /* SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HHmmssSSS");
               File file = new File("C:/JDBC/Output/" +
                 sdf.format(new Date()) + "_" +
@@ -267,6 +319,47 @@ public class Main {
 
         LOG.info("**********");
         return PgpEncryptDecrypt.encryptFile(file, certificate, "BC");
+    }
+
+    private static ConnectionDetails setSubscriberConfigSftpConnectionDetails() throws Exception {
+    // private static ConnectionDetails setSubscriberConfigSftpConnectionDetails(ExtractConfig config) {
+
+        // TODO Amend SFTP logon details as required
+        // Setting up the connection details
+
+        String hostname = "10.0.101.239";
+        // String hostname = config.getSftpConnectionDetails().getHostname();
+
+        int port = 22;
+        // int port = config.getSftpConnectionDetails().getPort();
+
+        String username = "endeavour";
+        // String username = config.getSftpConnectionDetails().getUsername();
+
+        String clientPrivateKey = null;
+        try {
+            clientPrivateKey = FileUtils.readFileToString(
+                    new File("C:/JDBC/SFTPKey/sftp02endeavour.ppk"), (String) null);
+
+        } catch (Exception ex) {
+            throw ex;
+        }
+        // String clientPrivateKey = config.getSftpConnectionDetails().getClientPrivateKey();
+
+        String clientPrivateKeyPassword = "";
+        // String clientPrivateKeyPassword = config.getSftpConnectionDetails().getClientPrivateKeyPassword();
+
+        String hostPublicKey = "";
+        // String hostPublicKey = config.getSftpConnectionDetails().getHostPublicKey();
+
+        ConnectionDetails sftpConnectionDetails = new ConnectionDetails();
+        sftpConnectionDetails.setHostname(hostname);
+        sftpConnectionDetails.setPort(port);
+        sftpConnectionDetails.setUsername(username);
+        sftpConnectionDetails.setClientPrivateKey(clientPrivateKey);
+        sftpConnectionDetails.setClientPrivateKeyPassword(clientPrivateKeyPassword);
+        sftpConnectionDetails.setHostPublicKey(hostPublicKey);
+        return sftpConnectionDetails;
     }
 
     private static void helperMethod() {
